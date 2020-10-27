966a535eadcb3987af8e2f500f6459da6ffd7f2a
Danelle Pontzious <dpontzious@gmail.com>
2020-10-27 11:47:45 -0700

    practicing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SucessAlertComponent } from './sucess-alert/sucess-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    WarningAlertComponent,
+    SucessAlertComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule
 
   ],
   providers: [],
